4348ba0065212518e3b2b5d466f92974369e096b
Jeffrey <oldenburger.jeffrey@gmail.com>
2020-07-04 22:46:59 +0200

    Removed wwise in main_menu

--- Scripts/menu_manager.cs
+++ Scripts/menu_manager.cs
@@ -9,7 +9,6 @@ public class menu_manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        AkSoundEngine.PostEvent("Theme_Song", gameObject);
 
 
     }
